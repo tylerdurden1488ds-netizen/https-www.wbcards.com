1eb22182577ae72a92860f69536b54f4f46a0ec7
tylerdurden1488ds-netizen <tylerdurden1488ds@gmail.com>
2026-08-03 21:10:04 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN не найден")
+
+if not GEMINI_API_KEY:
+    raise ValueError("GEMINI_API_KEY не найден")
